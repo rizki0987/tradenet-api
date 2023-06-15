8a02919e00e63ba572c93580ab2b954ecbc31650
rizki0987 <113367468+rizki0987@users.noreply.github.com>
2023-06-15 20:59:51 +0700

    port-change

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const Hapi = require('@hapi/hapi');
 const routes = require("./routes");
 const init = async () => {
     const server = Hapi.server({
-        port: 3389,
+        port: 80,
         host: 'localhost',
     });
     server.route(routes);
